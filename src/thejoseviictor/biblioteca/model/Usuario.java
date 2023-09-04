f492f4bcff7c492bb8b294e85c6f2693616e126d
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-09-04 17:55:00 -0300

    ➕build: Implementando Classe "Usuario" em "Model"

---
+++ src/thejoseviictor/biblioteca/model/Usuario.java
@@ -0,0 +1,17 @@
+package thejoseviictor.biblioteca.model;
+
+public class Usuario {
+    private String usuario;
+    private String senha;
+
+    public Usuario(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    /*
+    public boolean validaAcesso() {
+        return
+    }
+    */
+}
