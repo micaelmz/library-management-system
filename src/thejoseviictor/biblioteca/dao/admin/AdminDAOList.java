e1bc18615bace4cd02524341c4ef27b21f45f90f
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-09-19 11:05:34 -0300

    ➕build: Implementação do DAO "Admin"

---
+++ src/thejoseviictor/biblioteca/dao/admin/AdminDAOList.java
@@ -0,0 +1,57 @@
+package thejoseviictor.biblioteca.dao.admin;
+
+import thejoseviictor.biblioteca.model.Admin;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AdminDAOList implements AdminDAO{
+    private List<Admin> listaAdmins;
+
+    public AdminDAOList() {
+        this.listaAdmins = new LinkedList<Admin>();
+    }
+
+    @Override
+    public Admin criar(Admin objeto) {
+        if (!listaAdmins.contains(objeto)){
+            listaAdmins.add(objeto);
+        }
+        return objeto;
+    }
+
+    @Override
+    public List<Admin> lerTodos() {
+        return listaAdmins;
+    }
+
+    @Override
+    public Admin encontrarAdmin(Admin objeto) {
+        for (Admin percorrer : listaAdmins){
+            if (percorrer.getNome().equalsIgnoreCase(objeto.getNome())){
+                return percorrer;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Admin atualizar(Admin objeto) {
+        int indice = listaAdmins.indexOf(objeto);
+        if (indice != -1){
+            listaAdmins.set(indice, objeto);
+            return objeto;
+        }
+        return null;
+    }
+
+    @Override
+    public void deletar(Admin objeto) {
+        listaAdmins.remove(objeto);
+    }
+
+    @Override
+    public void deletarTodos() {
+        listaAdmins.clear();
+    }
+}
