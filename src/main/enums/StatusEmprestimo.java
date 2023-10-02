11003838162e739102b7c2bd7732dbfbf3f086bc
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-10-02 19:04:14 -0300

    📚docs: Comentários nas Classes "Cargo" e "StatusEmprestimo"

--- src/main/enums/StatusEmprestimo.java
+++ src/main/enums/StatusEmprestimo.java
@@ -1,5 +1,11 @@
 package main.enums;
 
+/**
+ * <b>Esta classe implementa os status dos empréstimos dos usuários do Sistema de Biblioteca</b>
+ * Portanto, contém constantes do tipo Enum, para os seguintes status: ATIVO, RESERVADO ou FINALIZADO.
+ *
+ * @author Micael Muniz
+ */
 public enum StatusEmprestimo {
     ATIVO,
     RESERVADO,
