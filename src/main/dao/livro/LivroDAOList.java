624d148c149f195af0c73d742645dce30a7baea1
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-10-02 15:44:28 -0300

    📚docs: Comentários na Interface "LivroDAO" e Classe "LivroDAOList"

--- src/main/dao/livro/LivroDAOList.java
+++ src/main/dao/livro/LivroDAOList.java
@@ -5,6 +5,16 @@ import main.model.Livro;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * <b>Esta interface implementa os métodos CRUD para livros</b>
+ * O método de armazenamento atual é LinkedList
+ *
+ * @author José Victor Oliveira
+ * @author Micael Muniz
+ *
+ * @see main.model.Livro
+ * @see main.dao.livro.LivroDAO
+ */
 public class LivroDAOList implements LivroDAO{
     private List<Livro> listaLivros;
 
@@ -12,6 +22,11 @@ public class LivroDAOList implements LivroDAO{
         this.listaLivros = new LinkedList<Livro>();
     }
 
+    /**
+     * Método que cria um novo livro
+     * @param objeto objeto do livro
+     * @return objeto do livro
+     */
     @Override
     public Livro criar(Livro objeto) {
         if (!listaLivros.contains(objeto)){
@@ -20,11 +35,20 @@ public class LivroDAOList implements LivroDAO{
         return objeto;
     }
 
+    /**
+     * Método que retorna todos os livros
+     * @return lista de livros
+     */
     @Override
     public List<Livro> lerTodos() {
         return listaLivros;
     }
 
+    /**
+     * Método que retorna um livro específico
+     * @param objeto objeto do livro
+     * @return retorna um livro específico
+     */
     @Override
     public Livro encontrarLivro(Livro objeto) {
         for (Livro percorrer : listaLivros){
@@ -35,6 +59,11 @@ public class LivroDAOList implements LivroDAO{
         return null;
     }
 
+    /**
+     * Método que atualiza os atributos de um livro específico
+     * @param objeto objeto do livro
+     * @return objeto do livro
+     */
     @Override
     public Livro atualizar(Livro objeto) {
         int indice = listaLivros.indexOf(objeto);
@@ -45,11 +74,18 @@ public class LivroDAOList implements LivroDAO{
         return null;
     }
 
+    /**
+     * Método que remove um livro específico
+     * @param objeto objeto do livro
+     */
     @Override
     public void deletar(Livro objeto) {
         listaLivros.remove(objeto);
     }
 
+    /**
+     * Método que remove todos os livros
+     */
     @Override
     public void deletarTodos() {
         listaLivros.clear();
