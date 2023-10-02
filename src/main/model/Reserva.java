a5edf57a5291d98c600d0846bdcf1f1eb8f1ed9a
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-10-02 14:50:42 -0300

    📚docs: Comentários na Classe "Reserva"

--- src/main/model/Reserva.java
+++ src/main/model/Reserva.java
@@ -2,6 +2,18 @@ package main.model;
 
 import java.util.LinkedList;
 
+/**
+ * <b>Esta classe implementa um lista de reservas de um livro</b>
+ * Possuindo os seguintes atributos:
+ * Livro associado
+ * Lista de leitores na reserva, esperando o livro ficar disponível
+ *
+ * @author José Victor Oliveira
+ * @author Micael Muniz
+ *
+ * @see main.model.Livro
+ * @see main.model.Leitor
+ */
 public class Reserva {
     private final Livro livro;
     private LinkedList<Leitor> reservas; // Esta lista irá armazenar objetos do tipo "Leitor" na fila de reserva.
@@ -11,14 +23,26 @@ public class Reserva {
         this.reservas = new LinkedList<Leitor>();
     }
 
+    /**
+     * Método que retorna o livro associado a reserva
+     * @return livro associado a reserva
+     */
     public Livro getLivro() {
         return livro;
     }
 
+    /**
+     * Método que retorna a lista de leitores na reserva
+     * @return lista de leitores na reserva
+     */
     public LinkedList<Leitor> getReservas() {
         return reservas;
     }
 
+    /**
+     * Método que adiciona um leitor a lista de reserva
+     * @param leitor objeto do leitor
+     */
     public void setReservas(Leitor leitor) {
         reservas.add(leitor);
     }
