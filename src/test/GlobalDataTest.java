18ef8a870a5ee969cd367fb5782f2cee16712e8f
Micael <micaelmuniz6@gmail.com>
2023-10-02 18:22:30 -0300

    🧪	test: adicionando testes de sessão / dados globais

---
+++ src/test/GlobalDataTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import main.model.Usuario;
+import main.GlobalData;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GlobalDataTest {
+
+    @Before
+    public void setUp() {
+        Usuario usuarioExemplo = new Usuario("johndoe", "123456", "John Doe");
+        usuarioExemplo.validaAcesso("johndoe", "123456");
+    }
+
+    @Test
+    public void usuarioAtualLogado() {
+        assertEquals("johndoe", GlobalData.getUsuarioAtual().getUsuario());
+    }
+
+
+}
