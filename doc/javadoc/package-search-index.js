56858d97bf7294cf06c0ae4d0dbce204f8826745
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-10-02 15:14:33 -0300

    📚docs: Atualização do JavaDocs

--- doc/javadoc/package-search-index.js
+++ doc/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main.model"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"main.model"}];updateSearchResults();
