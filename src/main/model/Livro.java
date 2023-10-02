4576dd4520484c9aa7a703f8922c08d97e9cbba8
Micael <micaelmuniz6@gmail.com>
2023-10-01 22:25:15 -0300

    ✨feat: adicionando metodos para manipular leitor o livro principalmente em relação a classe de Emprestimo

---
+++ src/main/model/Livro.java
@@ -0,0 +1,96 @@
+package main.model;
+
+public class Livro {
+    private final Integer id;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private Integer anoPublicacao;
+    private String categoria;
+    private int quantidade;
+    private boolean disponivel;
+    private Integer limiteRenovacoes;
+
+    public Livro(Integer id, String titulo, String autor, String isbn,
+                 Integer anoPublicacao, String categoria, int quantidade, Integer limiteRenovacoes) {
+
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.anoPublicacao = anoPublicacao;
+        this.categoria = categoria;
+        this.quantidade = quantidade;
+        this.disponivel = true;
+        this.limiteRenovacoes = limiteRenovacoes;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getISBN() {
+        return isbn;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public Integer getLimiteRenovacoes() {
+        return limiteRenovacoes;
+    }
+
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setISBN(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public void setLimiteRenovacoes(Integer limiteRenovacoes) {
+        this.limiteRenovacoes = limiteRenovacoes;
+    }
+}
