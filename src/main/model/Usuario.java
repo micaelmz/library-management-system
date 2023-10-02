6dc466f05116fa34b5eab51517fc93fc6b105451
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-10-02 13:04:03 -0300

    📚docs: Comentário na Classe "Usuario"

--- src/main/model/Usuario.java
+++ src/main/model/Usuario.java
@@ -2,6 +2,24 @@ package main.model;
 
 import main.GlobalData;
 
+/**
+ * <b>Esta classe implementa os usuários do Sistema de Biblioteca e seus atributos</b>
+ * Contendo as seguintes informações:
+ * ID do Usuário (Integer);
+ * Login de Usuário (String);
+ * Senha do Usuário (String);
+ * Nome Completo do Usuário (String);
+ * Cargo do Usuário (Cargo).
+ *
+ * Exemplo de Uso:
+ * Usuario usuario = new Usuario(id,"usuario", "senha", "nome");
+ *
+ * @author José Victor Oliveira
+ * @author Micael Muniz
+ *
+ * @see main.model.Cargo
+ */
+
 public class Usuario {
     private final Integer id;
     private String usuario;
@@ -16,43 +34,84 @@ public class Usuario {
         this.nome = nome;
     }
 
+    /**
+     * Método que retorna o nome do usuário
+     * @return nome do usuário
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Método que retorna o ID do usuário
+     * @return ID do usuário
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Método que retorna o login do usuário
+     * @return login do usuário
+     */
     public String getUsuario() {
         return usuario;
     }
 
+    /**
+     * Método que retorna a senha do usuário
+     * @return senha do usuário
+     */
     public String getSenha() {
         return senha;
     }
 
+    /**
+     * Método que retorna o cargo do usuário
+     * @return cargo do usuário
+     */
     public Cargo getCargo() {
         return cargo;
     }
 
-
+    /**
+     * Método que modifica o nome do usuário
+     * @param nome nome do usuário
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Método que modifica o login do usuário
+     * @param usuario login do usuário
+     */
     public void setUsuario(String usuario) {
         this.usuario = usuario;
     }
 
+    /**
+     * Método que modifica a senha do usuário
+     * @param senha senha do usuário
+     */
     public void setSenha(String senha) {
         this.senha = senha;
     }
 
+    /**
+     * Método que modifica o cargo do usuário
+     * @param cargo cargo do usuário
+     */
     public void setCargo(Cargo cargo) {
         this.cargo = cargo;
     }
 
+    /**
+     * Método que valida o login e senha do usuário, permitindo a entrada do usuário ao sistema
+     * @param usuario login do usuário
+     * @param senha senha do usuário
+     * @return dados corretos ou incorretos (true ou false)
+     */
     public boolean validaAcesso(String usuario, String senha) {
         if (this.usuario.equals(usuario) && this.senha.equals(senha)) {
             GlobalData.setUsuarioAtual(this);
