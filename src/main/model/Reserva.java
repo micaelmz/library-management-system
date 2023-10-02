1a6b0c6636e22016cfa288550b2372c0101f4f7f
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-10-02 12:23:12 -0300

    🔧chore: Correção na LinkedList "reservas" e Métodos "getReservas" e "setReservas"

--- src/main/model/Reserva.java
+++ src/main/model/Reserva.java
@@ -4,22 +4,22 @@ import java.util.LinkedList;
 
 public class Reserva {
     private final Livro livro;
-    private LinkedList<Integer> reservas; // Esta lista irá armazenar os "IDs" dos usuários na fila de reserva.
+    private LinkedList<Leitor> reservas; // Esta lista irá armazenar objetos do tipo "Leitor" na fila de reserva.
 
     public Reserva(Livro livro) {
         this.livro = livro;
-        this.reservas = new LinkedList<Integer>();
+        this.reservas = new LinkedList<Leitor>();
     }
 
     public Livro getLivro() {
         return livro;
     }
 
-    public LinkedList<Integer> getReservas() {
+    public LinkedList<Leitor> getReservas() {
         return reservas;
     }
 
-    public void setReservas(Integer leitor) {
+    public void setReservas(Leitor leitor) {
         reservas.add(leitor);
     }
 }
