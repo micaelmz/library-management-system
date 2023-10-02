ed5b8d3d51894c713f1386e930c9d6dbc61ad327
Micael <micaelmuniz6@gmail.com>
2023-10-02 18:23:37 -0300

    🚧 refactor: criando classe para futura refatoração e evitar a repetição de código por todas classes de DAO

--- src/main/dao/CRUD.java
+++ src/main/dao/CRUD.java
@@ -1,4 +1,15 @@
 package main.dao;
 
-public class CRUD {
+import main.model.Usuario;
+
+import java.util.List;
+
+public interface CRUD<T> {
+    public void criar(T objeto);
+
+    public List<T> lerTodos();
+    public T encontrarPorID(long id);
+    public void atualizar(T objeto);
+    public void deletar(T objeto);
+    public void deletarTodos();
 }
