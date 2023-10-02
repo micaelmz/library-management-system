5492da810756c3fb6fac8f251e7d852220efe0f3
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-10-02 15:50:31 -0300

    📚docs: Comentários na Interface "UsuarioDAO" e Classe "UsuarioDAOList"

--- src/main/dao/usuario/UsuarioDAOList.java
+++ src/main/dao/usuario/UsuarioDAOList.java
@@ -5,6 +5,16 @@ import main.model.Usuario;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * <b>Esta interface implementa os métodos CRUD para usuários</b>
+ * O método de armazenamento atual é LinkedList
+ *
+ * @author José Victor Oliveira
+ * @author Micael Muniz
+ *
+ * @see main.model.Usuario
+ * @see main.dao.usuario.UsuarioDAO
+ */
 public class UsuarioDAOList implements UsuarioDAO{
     private List<Usuario> listaUsuarios;
 
@@ -12,6 +22,11 @@ public class UsuarioDAOList implements UsuarioDAO{
         this.listaUsuarios = new LinkedList<Usuario>();
     }
 
+    /**
+     * Método que cria um novo usuário
+     * @param objeto objeto do usuário
+     * @return objeto do usuário
+     */
     @Override
     public Usuario criar(Usuario objeto){
         // Vai verificar se o objeto já existe na lista.
@@ -21,11 +36,20 @@ public class UsuarioDAOList implements UsuarioDAO{
         return objeto;
     }
 
+    /**
+     * Método que retorna todos os usuários
+     * @return lista de usuários
+     */
     @Override
     public List<Usuario> lerTodos(){
         return listaUsuarios;
     }
 
+    /**
+     * Método que retorna um usuário específico
+     * @param objeto objeto do usuário
+     * @return retorna um usuário específico
+     */
     @Override
     public Usuario encontrarUsuario(Usuario objeto) {
         for (Usuario percorrer : listaUsuarios){
@@ -36,6 +60,11 @@ public class UsuarioDAOList implements UsuarioDAO{
         return null;
     }
 
+    /**
+     * Método que atualiza os atributos de um usuário específico
+     * @param objeto objeto do usuário
+     * @return objeto do usuário
+     */
     @Override
     public Usuario atualizar(Usuario objeto) {
         int indice = listaUsuarios.indexOf(objeto);
@@ -47,11 +76,18 @@ public class UsuarioDAOList implements UsuarioDAO{
         return null;
     }
 
+    /**
+     * Método que remove um usuário específico
+     * @param objeto objeto do usuário
+     */
     @Override
     public void deletar(Usuario objeto) {
         listaUsuarios.remove(objeto);
     }
 
+    /**
+     * Método que remove todos os usuários
+     */
     @Override
     public void deletarTodos() {
         listaUsuarios.clear();
