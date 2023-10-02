026de8f861a3b9b1fc1b0e78ec4960e9c24e6b1c
Micael <micaelmuniz6@gmail.com>
2023-10-02 18:15:01 -0300

    ✨ feat: adicionando responsabilidade de definição de ID de um objeto para o DAO, onde o mesmo gera IDs unicos e armazena o ultimo ID

--- src/main/dao/livro/LivroDAO.java
+++ src/main/dao/livro/LivroDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface LivroDAO {
     public Livro criar(Livro objeto);
     public List<Livro> lerTodos();
-    public Livro encontrarLivro(Livro objeto);
+    public Livro encontrarPorID(Integer id);
     public Livro atualizar(Livro objeto);
     public void deletar(Livro objeto);
     public void deletarTodos();
