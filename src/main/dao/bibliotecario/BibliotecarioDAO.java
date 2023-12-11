44ffb8add8065c408fd4a5353097a25546715b31
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-12-11 01:20:18 -0300

    📚 docs: Atualização nos Comentários de "BibliotecarioDAO" e "BibliotecarioDAOList"

--- src/main/dao/bibliotecario/BibliotecarioDAO.java
+++ src/main/dao/bibliotecario/BibliotecarioDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 /**
  * <b>Esta interface implementa os métodos CRUD para bibliotecários</b>
- * O método de armazenamento atual é LinkedList
+ * Os métodos de armazenamento atuais são os arquivos binários ".dat"
  *
  * @author José Victor Oliveira
  * @author Micael Muniz
--- src/main/dao/bibliotecario/BibliotecarioDAOList.java
+++ src/main/dao/bibliotecario/BibliotecarioDAOList.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 /**
  * <b>Esta classe implementa os métodos CRUD para bibliotecários</b>
- * O método de armazenamento atual é LinkedList
+ * Os métodos de armazenamento atuais são os arquivos binários ".dat"
  *
  * @author José Victor Oliveira
  * @author Micael Muniz
