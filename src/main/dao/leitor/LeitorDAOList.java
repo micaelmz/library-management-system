e45a148a3947b903db24b9001e4237fa949b0613
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2023-12-11 01:29:04 -0300

    🗃️raw: Salvando em Arquivos Binários os Objetos de "Leitor"

--- src/main/dao/leitor/LeitorDAOList.java
+++ src/main/dao/leitor/LeitorDAOList.java
@@ -1,14 +1,14 @@
 package main.dao.leitor;
 
+import main.dao.PastaArquivos;
 import main.model.Leitor;
-import main.model.Livro;
-
+import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * <b>Esta interface implementa os métodos CRUD para leitores</b>
- * O método de armazenamento atual é LinkedList
+ * <b>Esta classe implementa os métodos CRUD para leitores</b>
+ * Os métodos de armazenamento atuais são os arquivos binários ".dat"
  *
  * @author José Victor Oliveira
  * @author Micael Muniz
@@ -19,10 +19,32 @@ import java.util.List;
 public class LeitorDAOList implements LeitorDAO{
     private List<Leitor> listaLeitores;
     private Integer ultimoID = 0;
-
+    private String destinoArquivo = System.getProperty("user.dir") + "\\files\\Leitores.dat";
 
     public LeitorDAOList() {
         this.listaLeitores = new LinkedList<Leitor>();
+    }
+
+    /**
+     * Método que carrega os Leitores do arquivo binário "Leitores.dat" para a lista "listaLeitores".
+     */
+    @Override
+    public void carregarArquivo() throws IOException, ClassNotFoundException {
+        FileInputStream arquivoEntrar = new FileInputStream(destinoArquivo);
+        ObjectInputStream ler = new ObjectInputStream(arquivoEntrar);
+        listaLeitores = (LinkedList<Leitor>) ler.readObject();
+    }
+
+    /**
+     * Método que salva os Leitores da lista "listaLeitores" para o arquivo binário "Leitores.dat".
+     */
+    @Override
+    public void salvarArquivo() throws IOException{
+        PastaArquivos existePastaArquivos = new PastaArquivos();
+        existePastaArquivos.verificarPastaArquivos();
+        FileOutputStream arquivoSair = new FileOutputStream(destinoArquivo);
+        ObjectOutputStream salvar = new ObjectOutputStream(arquivoSair);
+        salvar.writeObject(listaLeitores);
     }
 
     /**
