c63324aa734827f4f684655040c02b785508c6ef
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2024-02-17 16:19:20 -0300

    🧱ci: Update do ProfileView e Controller

--- src/main/java/app/controllers/ProfileController.java
+++ src/main/java/app/controllers/ProfileController.java
@@ -1,16 +1,39 @@
 package app.controllers;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import app.GlobalData;
+import app.dao.baseuser.BaseUserDAOList;
+import app.model.BaseUser;
+import app.views.LoginView;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class ProfileController extends Application {
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+public class ProfileController {
+    @FXML TextField fnameInput;
+    @FXML TextField lnameInput;
+    @FXML TextField usernameInput;
+    @FXML PasswordField passwordInput;
+    @FXML PasswordField confirmPasswordInput;
 
-    @Override
-    public void start(Stage primaryStage) {
+    BaseUserDAOList allUsers = new BaseUserDAOList();
 
+    BaseUser loggedUser = GlobalData.getLoggedUser();
+
+    Boolean erro = false;
+
+    @FXML
+    protected void onSaveButton() throws IOException, ClassNotFoundException{
+        allUsers.loadDatFile();
+        BaseUser oldUserData = allUsers.findByUsername(loggedUser.getUsername());
+        String fullName = fnameInput.getText() + ' ' + lnameInput.getText();
+        String username = usernameInput.getText();
+        String password = passwordInput.getText();
+        loggedUser.setName(fullName);
+        loggedUser.setUsername(username);
+        loggedUser.setPassword(password);
+        allUsers.update(oldUserData, loggedUser);
+        allUsers.saveDatFile();
     }
 }
