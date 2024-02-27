0992c315562414b9f78c0c92b3f0922eca6e29fa
Micael <micaelmuniz6@gmail.com>
2024-02-27 20:09:29 -0300

    ✨ feat. funcionalide de criar livros

--- src/main/java/app/controllers/AddBookController.java
+++ src/main/java/app/controllers/AddBookController.java
@@ -1,13 +1,46 @@
 package app.controllers;
 
+import app.dao.BookDAOList;
+import app.model.Book;
 import app.views.DashboardView;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class AddBookController {
 
     @FXML
-    protected void onSaveButton() {
+    TextField titleInput;
+    @FXML
+    TextField authorInput;
+    @FXML
+    TextField categoryInput;
+    @FXML
+    TextField yearInput;
+    @FXML
+    TextField isbnInput;
+    @FXML
+    TextField quantityInput;
+    @FXML
+    TextField renewLimitInput;
 
+    @FXML
+    Label statusLabel;
+
+    @FXML
+    protected void onSaveButton() throws IOException, ClassNotFoundException {
+        if (titleInput.getText().isEmpty() || authorInput.getText().isEmpty() || categoryInput.getText().isEmpty() || yearInput.getText().isEmpty() || isbnInput.getText().isEmpty() || quantityInput.getText().isEmpty() || renewLimitInput.getText().isEmpty()) {
+            statusLabel.setText("Por favor preencha todos os campos");
+            return;
+        }
+        BookDAOList bookDAO = new BookDAOList();
+        bookDAO.loadDatFile();
+        bookDAO.create(new Book(titleInput.getText(), authorInput.getText(), isbnInput.getText(), Integer.parseInt(yearInput.getText()), categoryInput.getText(), Integer.parseInt(quantityInput.getText()), Integer.parseInt(renewLimitInput.getText())));
+        bookDAO.saveDatFile();
+
+        DashboardView.show();
     }
 
     @FXML
